48938797f354875c44763ef50b06a640efebefaa
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2015-11-01 20:09:04 -0600

    ActionSearch view

--- app/src/main/java/com/s10r/photosearch/PhotoGrid.java
+++ app/src/main/java/com/s10r/photosearch/PhotoGrid.java
@@ -1,9 +1,12 @@
 package com.s10r.photosearch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class PhotoGrid extends AppCompatActivity {
 
@@ -17,8 +20,24 @@ public class PhotoGrid extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_photo_grid, menu);
-        return true;
-    }
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                TextView tvText = (TextView)findViewById(R.id.tvText);
+                tvText.setText(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+   }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -27,10 +46,7 @@ public class PhotoGrid extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
